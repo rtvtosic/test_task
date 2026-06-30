90157764172d73d7443930a22fe899db4701a17d
cactus3106@gmail.com <anton3105@bk.ru>
2026-06-30 21:18:27 +0300

    Добавлено подключение к Elasticsearch

---
+++ elastic_conn.py
@@ -0,0 +1,34 @@
+import os
+
+from dotenv import load_dotenv
+from elasticsearch import Elasticsearch
+
+"""
+Структура Индекса:
+
+id - id из базы;
+text - текст из структуры БД.
+"""
+
+load_dotenv()
+elastic_host = os.getenv("ELASTIC_HOST")
+elastic_port = os.getenv("ELASTIC_PORT")
+
+# ==== подключение к elasticsearch ====
+client = Elasticsearch(
+    hosts=[f"http://{elastic_host}:{elastic_port}"]
+)
+
+# ==== создание индекса ====
+mappings = {
+    "properties": {
+        "id": {"type": "integer"},
+        "text": {"type": "text"}
+    }
+}
+
+# client.indices.delete(index="documents")
+
+if not client.indices.exists(index="documents"):
+    client.indices.create(index="documents", mappings=mappings)
+

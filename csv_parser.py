f5b5f10d4877615efd3766d9a6ba63b0892a8da1
cactus3106@gmail.com <anton3105@bk.ru>
2026-07-01 10:10:55 +0300

    Обновлена работа парсера

--- csv_parser.py
+++ csv_parser.py
@@ -1,3 +1,4 @@
+import ast
 import pandas as pd
 from datetime import datetime
 
@@ -5,25 +6,21 @@ from datetime import datetime
 def parse_data(path: str) -> list:
     data = pd.read_csv(path)
     formatted_data = []
-    id = 0
 
     for row in data.itertuples():
         text = row.text
         created_date = row.created_date
         rubrics = row.rubrics
 
-        # преобразование created_date в datetime
         formatted_created_date = datetime.strptime(created_date, "%Y-%m-%d %H:%M:%S")
+        formatted_rubrics = ast.literal_eval(rubrics)
 
-        # преобразование rubrics из str в list[str]
-        formatted_rubrics = [x.strip("'") for x in rubrics[1:-1].split(', ')]
-        formatted_data.append((id, text, formatted_created_date, formatted_rubrics))
-        id += 1
-    
+        formatted_data.append((text, formatted_created_date, formatted_rubrics))
+
     return formatted_data
 
 
 if __name__ == "__main__":
     # data = parse_data('posts.csv')
     # print(data)
-    print(parse_data('posts.csv'))
+    parse_data('posts.csv')

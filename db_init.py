128bbeda681d683583464089bea439d07f7f57f0
cactus3106@gmail.com <anton3105@bk.ru>
2026-07-01 10:04:12 +0300

    Обновлен процесс заполнения БД

--- db_init.py
+++ db_init.py
@@ -1,6 +1,10 @@
 import os
-from sqlalchemy import create_engine
+
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import Session
 from models import Base, Document
+
+from csv_parser import parse_data
 from dotenv import load_dotenv
 
 
@@ -15,4 +19,24 @@ engine = create_engine(f"postgresql+psycopg://{user}:{password}@{host}/{database
 
 # создание таблиц в базе данных
 Base.metadata.create_all(bind=engine)
-print("База данных и таблицы успешно созданы")
+
+# загрузка данных из csv-файла в правильном формате
+data = parse_data(path='posts.csv')
+
+# создание сессии и загрузка данных в БД
+try:
+    with Session(bind=engine) as db:
+        # удаление данных перед загрузкой
+        db.execute(text("TRUNCATE TABLE document RESTART IDENTITY"))
+        db.commit()
+
+        for obj in data:
+            db.add(
+                Document(text=obj[0],
+                        created_date=obj[1],
+                        rubrics=obj[2])
+                )
+        db.commit() # сохранение изменений в БД
+    print("База данных успешно заполнена")
+except Exception as e:
+    print(f"Возникла ошибка: {e}")
